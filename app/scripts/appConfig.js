c207f50b1ab82abcb678200aa179458407b285b2
marcin555 <macq144@gmail.com>
2016-04-13 10:30:09 +0200

    Update appConfig.js

--- app/scripts/appConfig.js
+++ app/scripts/appConfig.js
@@ -1,7 +1,1 @@
-(function () {
-    'use strict';
 
-    angular.module('utcApp').config(function (paginationSupportProvider) {
-        paginationSupportProvider.setDefaultConfig({maxResultsProperty: 'size', firstResultProperty: 'from'});
-    });
-})();
